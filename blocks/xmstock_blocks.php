e344f81981b55aab06293642a3ccd67d043a37fc
GregMage <gregory.mage@gmail.com>
2023-01-27 19:04:31 +0100

    fix: https://github.com/GregMage/xmstock/issues/19 Ajout des blocs

--- blocks/xmstock_blocks.php
+++ blocks/xmstock_blocks.php
@@ -18,12 +18,17 @@
  */
 use Xmf\Module\Helper;
 function block_xmstock_show($options) {
+	global $xoopsUser;
 	include __DIR__ . '/../include/common.php';
 	include_once __DIR__ . '/../class/utility.php';
 
 	$helper = Helper::getHelper('xmstock');
 	$helper->loadLanguage('main');
 
+	// Get Permission to manage
+	$managePermissionArea = XmstockUtility::getPermissionArea('xmstock_manage');
+
+	$block = array();
 	$criteria = new CriteriaCompo();
 	$criteria->setSort('order_dorder DESC, order_status');
 	$criteria->setOrder('ASC');
@@ -31,131 +36,52 @@ function block_xmstock_show($options) {
 	switch ($options[2]) {
 		case "myorders":
 			$criteria->add(new Criteria('order_userid', !empty($xoopsUser) ? $xoopsUser->getVar('uid') : 0));
+			$block['type'] = 'myorders';
 			break;
 
 		case "process":
-			//$criteria->add(new Criteria('order_userid', !empty($xoopsUser) ? $xoopsUser->getVar('uid') : 0));
+			if (!empty($managePermissionArea)) {
+				$criteria->add(new Criteria('order_areaid', '(' . implode(',', $managePermissionArea) . ')', 'IN'));
+			}
+			$block['type'] = 'process';
 			break;
 
 	}
 	$criteria->add(new Criteria('order_status', '(' . $options[0] . ')', 'IN'));
-
-	/*// Get Permission to view
-	$viewPermissionCat = XmdocUtility::getPermissionCat('xmdoc_view');
-	//xmsocial
-	if (xoops_isActiveModule('xmsocial') && $helper->getConfig('general_xmsocial', 0) == 1) {
-		xoops_load('utility', 'xmsocial');
-	}
-
-	$permDocHelper = new Helper\Permission('xmdoc');
-
-	$block = array();
-	$block['use_modal'] = $helper->getConfig('general_usemodal', 1);
-
-	$criteria = new CriteriaCompo();
-	$criteria->add(new Criteria('document_status', 1));
-	switch ($options[2]) {
-        case "date":
-			$criteria->setSort('document_date DESC, document_name');
-			$criteria->setOrder('ASC');
-        break;
-
-        case "hits":
-			$criteria->setSort('document_counter DESC, document_name');
-			$criteria->setOrder('ASC');
-        break;
-
-        case "rating":
-			$criteria->setSort('document_rating DESC, document_name');
-			$criteria->setOrder('ASC');
-        break;
-
-        case "random":
-            $criteria->setSort('RAND()');
-        break;
-
-		case "title":
-			switch ($options[3]) {
-				case 0:
-					$criteria->setSort('document_date DESC, document_name');
-					$criteria->setOrder('ASC');
-					break;
-
-				case 1:
-					$criteria->setSort('document_counter DESC, document_name');
-					$criteria->setOrder('ASC');
-					break;
-
-				case 2:
-					$criteria->setSort('document_rating DESC, document_name');
-					$criteria->setOrder('ASC');
-					break;
-
-				case 3:
-					$criteria->setSort('RAND()');
-					break;
-			}
-			$block['size'] = $options[4];
-			$block['logo'] = $options[5];
-			break;
-    }
-	$category_ids = explode(',', $options[0]);
-	if (!in_array(0, $category_ids)) {
-        $criteria->add(new Criteria('category_id', '(' . $options[0] . ')', 'IN'));
-    }
 	$criteria->setLimit($options[1]);
-	if (!empty($viewPermissionCat)) {
-		$criteria->add(new Criteria('document_category', '(' . implode(',', $viewPermissionCat) . ')', 'IN'));
-	}
-	$documentHandler->table_link = $documentHandler->db->prefix("xmdoc_category");
-	$documentHandler->field_link = "category_id";
-	$documentHandler->field_object = "document_category";
-	$document_arr = $documentHandler->getByLink($criteria);
-	if (count($document_arr) > 0 && !empty($viewPermissionCat)) {
-		foreach (array_keys($document_arr) as $i) {
-			$document_id                   = $document_arr[$i]->getVar('document_id');
-			$document['id']                = $document_id;
-			$document['name']              = $document_arr[$i]->getVar('document_name');
-			$document['category']          = $document_arr[$i]->getVar('category_name');
-			$document['categoryid']        = $document_arr[$i]->getVar('document_category');
-			$document['document']          = $document_arr[$i]->getVar('document_document');
-			$document['description']       	   = str_replace('[break]', '', $document_arr[$i]->getVar('document_description', 'show'));
-			if (false == strpos($document_arr[$i]->getVar('document_description', 'e'), '[break]')){
-				$document['description_short'] = $document_arr[$i]->getVar('document_description', 'show');
-				$document['description_end']   = '';
-			}else{
-				$document['description_short'] = substr($document_arr[$i]->getVar('document_description', 'show'), 0, strpos($document_arr[$i]->getVar('document_description', 'show'),'[break]'));
-				$document['description_end']   = str_replace('[break]', '', substr($document_arr[$i]->getVar('document_description', 'show'), strpos($document_arr[$i]->getVar('document_description', 'show'),'[break]')));
+	$order_arr = $orderHandler->getall($criteria);
+	if (!empty($order_arr)) {
+		foreach (array_keys($order_arr) as $i) {
+			$order['id']              = $order_arr[$i]->getVar('order_id');
+			$order['ddesired']        = formatTimestamp($order_arr[$i]->getVar('order_ddesired'), 's');
+			$order['dorder']          = formatTimestamp($order_arr[$i]->getVar('order_dorder'), 's');
+			switch ($order_arr[$i]->getVar('order_status')) {
+				case 1:
+					$order['status_text'] = _MA_XMSTOCK_ORDER_STATUS_1;
+					$order['status_icon'] = '<span class="fa fa-hourglass-start fa-fw" aria-hidden="true"></span>';
+					break;
+				case 2:
+					$order['status_text'] = _MA_XMSTOCK_ORDER_STATUS_2;
+					$order['status_icon'] = '<span class="fa fa-hourglass-half fa-fw" aria-hidden="true"></span>';
+					break;
+				case 3:
+					$order['status_text'] = _MA_XMSTOCK_ORDER_STATUS_3;
+					$order['status_icon'] = '<span class="fa fa-thumbs-o-up fa-fw" aria-hidden="true"></span>';
+					break;
+				case 4:
+					$order['status_text'] = _MA_XMSTOCK_ORDER_STATUS_4;
+					$order['status_icon'] = '<span class="fa fa-check fa-fw" aria-hidden="true"></span>';
+					break;
+				case 0:
+					$order['status_text'] = _MA_XMSTOCK_ORDER_STATUS_0;
+					$order['status_icon'] ='<span class="fa fa-ban fa-fw" aria-hidden="true"></span>';
+					break;
 			}
-			$document['size']              = XmdocUtility::SizeConvertString($document_arr[$i]->getVar('document_size'));
-			$document['author']            = XoopsUser::getUnameFromId($document_arr[$i]->getVar('document_userid'));
-			$document['date']              = formatTimestamp($document_arr[$i]->getVar('document_date'), 's');
-			if ($document_arr[$i]->getVar('document_mdate') != 0) {
-				$document['mdate']         = formatTimestamp($document_arr[$i]->getVar('document_mdate'), 's');
-			}
-			$document['counter']           = $document_arr[$i]->getVar('document_counter');
-			$document['showinfo']          = $document_arr[$i]->getVar('document_showinfo');
-			$document_img                  = $document_arr[$i]->getVar('document_logo') ?: 'blank_doc.gif';
-			$document['logo']              = $url_logo_document . $document_img;
-			$color 						   = $document_arr[$i]->getVar('category_color');
-			if ($color == '#ffffff'){
-				$document['color'] 		   = false;
-			} else {
-				$document['color'] 		   = $color;
-			}
-			$document['perm_edit']         = $permDocHelper->checkPermission('xmdoc_editapprove', $document['categoryid']);
-			$document['perm_del']          = $permDocHelper->checkPermission('xmdoc_delete', $document['categoryid']);
-			//xmsocial
-			if (xoops_isActiveModule('xmsocial') && $helper->getConfig('general_xmsocial', 0) == 1) {
-				$document['xmsocial_arr'] = XmsocialUtility::renderRating('xmdoc', $document_id , 5, $document_arr[$i]->getVar('document_rating'), $document_arr[$i]->getVar('document_votes'));
-				$document['dorating'] = 1;
-			} else {
-				$document['dorating'] = 0;
-			}
-			$block['document'][] = $document;
-			unset($document);
+			$block['order'][] = $order;
+			unset($order);
+			unset($order_1);
 		}
-	}*/
+	}
 	$GLOBALS['xoTheme']->addStylesheet(XOOPS_URL . '/modules/xmstock/assets/css/styles.css');
 	return $block;
 }
