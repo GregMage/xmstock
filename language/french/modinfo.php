8cd45250d57dd9b0e390d5366d0764805192087e
GregMage <gregory.mage@gmail.com>
2023-02-01 20:58:54 +0100

    oubli fr

--- language/french/modinfo.php
+++ language/french/modinfo.php
@@ -22,7 +22,7 @@ define('_MI_XMSTOCK_DESC', 'Gestion de stock');
 
 // Menu
 define('_MI_XMSTOCK_MENU_HOME', 'Index');
-define('_MI_XMSTOCK_MENU_AREA', 'Zone');
+define('_MI_XMSTOCK_MENU_AREA', 'Zone de stockage');
 define('_MI_XMSTOCK_MENU_OUTPUT', 'Sorties');
 define('_MI_XMSTOCK_MENU_STOCK', 'Stock');
 define('_MI_XMSTOCK_MENU_TRANSFER', 'Transferts');
