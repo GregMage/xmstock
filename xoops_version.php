5112836d656abefd34a395b993f93258ec00bfd8
GregMage <gregory.mage@gmail.com>
2024-09-25 15:51:10 +0200

    version 1.1.1

--- xoops_version.php
+++ xoops_version.php
@@ -18,7 +18,7 @@
  */
 $modversion['dirname']     = basename(__DIR__);
 $modversion['name']        = ucfirst(basename(__DIR__));
-$modversion['version']     = '1.1.0-Stable';
+$modversion['version']     = '1.1.1-Stable';
 $modversion['description'] = _MI_XMSTOCK_DESC;
 $modversion['author']      = 'Grégory Mage (Mage)';
 $modversion['url']         = 'https://github.com/GregMage';
@@ -273,7 +273,7 @@ $modversion['config'][] = [
 ];
 
 // About stuff
-$modversion['release_date']  = '2024/09/16';
+$modversion['release_date']  = '2024/09/25';
 
 $modversion['developer_lead']      = 'Mage';
 $modversion['module_website_url']  = 'github.com/GregMage';
