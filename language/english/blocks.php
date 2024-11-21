62ff0e5dc2595332a75b11aa9fdcb90882d8cfb5
mambax7 <mambax7@gmail.com>
2024-11-21 14:04:33 -0500

    English

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -16,18 +16,18 @@
  * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
  * @author          Mage Gregory (AKA Mage)
  */
-define('_MB_XMSTOCK_AREA', 'Zone de stockage à afficher:');
-define('_MB_XMSTOCK_ALLAREA', 'Afficher toutes les zones de stockage');
-define('_MB_XMSTOCK_NBARTICLES', 'Nombre d\'articles à afficher');
-define('_MB_XMSTOCK_NBLOANS', 'Nombre of loans to display');
+define('_MB_XMSTOCK_AREA', 'Storage area to display:');
+define('_MB_XMSTOCK_ALLAREA', 'Show all storage areas');
+define('_MB_XMSTOCK_NBARTICLES', 'Number of items to display');
+define('_MB_XMSTOCK_NBLOANS', 'Number of loans to display');
 define('_MB_XMSTOCK_NBORDER', 'Number of orders to display');
 define('_MB_XMSTOCK_NBTRANSFERT', 'Number of transfert to display');
-define('_MB_XMSTOCK_NOLOAN', 'Pas d\'emprunt à afficher');
-define('_MB_XMSTOCK_NOORDER', 'Pas de commande à afficher');
-define('_MB_XMSTOCK_NOOVERDRAFT', 'Pas d\'articles en dessous du montant minimum');
-define('_MB_XMSTOCK_NOTRANSFER', 'Pas de transfert en attente de validation');
-define('_MB_XMSTOCK_SORTAMOUNT', 'Tri par montant:');
-define('_MB_XMSTOCK_SORTDATE', 'Tri par date:');
+define('_MB_XMSTOCK_NOLOAN', 'No loans to display');
+define('_MB_XMSTOCK_NOORDER', 'No orders to display');
+define('_MB_XMSTOCK_NOOVERDRAFT', 'No items below minimum amount');
+define('_MB_XMSTOCK_NOTRANSFER', 'No transfers awaiting validation');
+define('_MB_XMSTOCK_SORTAMOUNT', 'Sort by amount:');
+define('_MB_XMSTOCK_SORTDATE', 'Sort by date:');
 define('_MB_XMSTOCK_STATUS', 'Display status:');
-define('_MB_XMSTOCK_STATUS_VALID', 'Validé');
-define('_MB_XMSTOCK_STATUS_WAITING', 'En attente');
+define('_MB_XMSTOCK_STATUS_VALID', 'Validated');
+define('_MB_XMSTOCK_STATUS_WAITING', 'Waiting');
